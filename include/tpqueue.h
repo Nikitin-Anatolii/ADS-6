a6fe13728de82c13b0a5cd98ed4adeeaaccbe37c
Nikitin-Anatolii <99128452+Nikitin-Anatolii@users.noreply.github.com>
2022-04-16 19:48:20 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,26 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+ private:
+   struct SYM arr[size];
+   int i = 0;
+ public:
+   void push() {
+     if (i < size) {
+       for (int j = i; j > -1; j--) {
+         if (j && (item.prior <= arr[j - 1].prior))
+           arr[j] = arr[j - 1];
+         else {
+           arr[j] = item;
+           break;
+         }
+       }
+     }
+   }
+   SYM pop() {
+     if (i > 0)
+       return arr[--i];
+   }
 };
 
 struct SYM {
